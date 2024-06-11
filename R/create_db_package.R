9ca097ae6a5bbb8d407f00762b3eeca8f127d1d0
davidfoord1 <140633967+davidfoord1@users.noreply.github.com>
2024-06-11 12:48:41 +0100

    Remove functions no longer used
    remove_db_package function, dbp_get_db_structure documentation.
    
    create_list_tables_fun to be replaced.

--- R/create_db_package.R
+++ R/create_db_package.R
@@ -10,14 +10,14 @@ create_db_package_dir <- function(path) {
 create_db_package_desc <- function(package_name, path) {
   description_path <- file.path(path, "DESCRIPTION")
 
-  description_content <- paste(
+  description_content <- paste0(
     "Package: ", package_name, "\n",
     "Version: 0.1.0\n",
     "Title: Temporary DB Package\n",
     "Description: A package built with dbpkgr as an interface to a database connection.\n",
     "Encoding: UTF-8\n",
     "Depends: R (>= 3.5.0)\n",
-    sep = ""
+    "Imports: dbpkgr, DBI"
   )
 
   writeLines(description_content, con = description_path)
@@ -31,27 +31,4 @@ create_db_package_path <- function(package_name, temporary) {
   } else {
     file.path("dkpkgr", package_name)
   }
-}
-
-#' Uninstall and delete a dbpkgr db package
-#'
-#' @param package_name The name of the package to be deleted.
-#' @param path The path to the package files.
-#'
-#' @return
-#' Returns `NULL` invisibly.
-remove_db_package <- function(package_name) {
-  path <- dbp_package_path(package_name)
-
-  tryCatch({
-    detach(paste0("package:", package_name), unload = TRUE, character.only = TRUE)
-  }, error = function(e) {
-    message(paste0("Package not loaded package:", package_name, " - ", e$message))
-  })
-
-  if (dir.exists(path)) {
-    unlink(path, recursive = TRUE, force = TRUE)
-  }
-
-  invisible()
 }
